0ee50ba69b8799efb3be2b777c0cbe498f72516e
Elijah Haley <elijahhaley99@gmail.com>
2022-03-27 13:45:07 -0700

    init

---
+++ test.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/test')
+def lil():
+        return "Test successful Like You my sir"    
+
+
+@app.route('/')
+def index():
+        return render_template("index.html", row=8 , col=8 , color1 = 'red', color2='black')
+
+
+@app.route('/<int:x>')
+def row(x):
+        return render_template("index.html", row=x , col=8 , color1 = 'red', color2='black')
+
+
+@app.route('/<int:x>/<int:y>/<string:one>')
+def row_col_color(x,y,one):
+        return render_template("index.html", row=x , col=y , color1 = one, color2='black')
+
+@app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
+def row_col_color2(x,y,one,two):
+        return render_template("index.html", row=x , col=y , color1 = one, color2=two)
+
+
+
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
